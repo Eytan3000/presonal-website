6259132667e77dcaed9b91e15108fd846f0e42b6
Eytan-Krief <eytan@roundforest.com>
2026-02-28 20:19:56 +0200

    changed hero text

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,13 +1,13 @@
-import './hero.css';
+import "./hero.css";
 
-import { FaGithub } from 'react-icons/fa';
-import { FaLinkedin } from 'react-icons/fa';
-import { openWindow } from '../../helper_functions/helper_functions';
-import { useMobile } from '../../Contexts/MobileProvider';
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { openWindow } from "../../helper_functions/helper_functions";
+import { useMobile } from "../../Contexts/MobileProvider";
 
-const meopLink = 'https://mepo.info/';
-const gitHubLink = 'https://github.com/Eytan3000';
-const linkedInLink = 'https://www.linkedin.com/in/eytan-krief/';
+const roundforestLink = "https://www.roundforest.com/";
+const gitHubLink = "https://github.com/Eytan3000";
+const linkedInLink = "https://www.linkedin.com/in/eytan-krief/";
 
 export default function Hero() {
   const { isMobile } = useMobile();
@@ -21,27 +21,29 @@ export default function Hero() {
     <div
       className="hero"
       style={{
-        marginTop: isMobile ? '0' : '',
-        marginBottom: '25%',
-      }}>
+        marginTop: isMobile ? "0" : "",
+        marginBottom: "25%",
+      }}
+    >
       <p className="code-text">Hi, my name is</p>
       <h1>Eytan Krief</h1>
       <h2 className="hero-secondary-title">
-        I’m a <b>Full-Stack developer</b> specializing in building exceptional
-        digital experiences. Currently, I’m focused on building human-centered
-        products at{' '}
-        <a target="_blank" href={meopLink} className="mepo-link">
-          Mepo
+        I’m a <b>Backend developer</b> specializing in building scalable systems
+        and APIs. Currently, I’m focused on the technology that powers shopping
+        simplified at{" "}
+        <a target="_blank" href={roundforestLink} className="mepo-link">
+          Roundforest
         </a>
         .
       </h2>
       {/* <button className="cv-button">CV</button> */}
       <div
         style={{
-          display: 'flex',
-          gap: '30px',
-          marginTop: '30px',
-        }}>
+          display: "flex",
+          gap: "30px",
+          marginTop: "30px",
+        }}
+      >
         <FaGithub
           className="hero-social-icon"
           onClick={handleGithubClick}
